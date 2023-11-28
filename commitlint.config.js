868e18f79584764b21b89d91a199cd3bc646dd72
Howard HILL <139238193+howard-oc@users.noreply.github.com>
2023-11-28 09:55:14 +0000

    ci: Create commitlint.yaml (#7)
    * ci: fixed linting

---
+++ commitlint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["build", "ci", "docs", "feat", "fix", "perf", "refactor", "test"],
+    ],
+
+    "subject-case": [2, "always", "lower-case"],
+
+    "subject-empty": [2, "never"],
+
+    "subject-full-stop": [2, "never", "."],
+  },
+
+  prompt: {
+    settings: {
+      enable: true,
+      maxSubjectLength: 80,
+    },
+  },
+
+  messages: {
+    "subject-empty": "Subject cannot be empty",
+
+    "subject-full-stop": "Subject cannot end with period",
+
+    dco: "Commits should be signed off",
+  },
+};
